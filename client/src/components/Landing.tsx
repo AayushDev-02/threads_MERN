c77483f3f35c9d09be59d197be138189fef1eb0a
Aayush Yadav <yadavaayush002@gmail.com>
2023-12-31 04:12:28 +0530

    Added theme change button

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,24 +1,27 @@
-import logo from "/logo.svg"
+import { useEffect, useState } from "react"
+import { useTheme } from "./theme-provider"
 
 
 const Landing = () => {
+    const {theme} = useTheme()
+    const [logoUrl , setLogoUrl] = useState<string>("")
+    useEffect(() => {
+        if(theme === "light"){
+            setLogoUrl("/logo-color.svg")
+        } else{
+            setLogoUrl("/logo.svg")
+        }
+    }, [theme]) 
     return (
-        <div 
-        style={{
-            background: "radial-gradient(64.09% 64.09% at 49.2% 38.61%, rgba(34, 34, 34, 0.00) 0%, #000 100%)"
-        }}
-        className=' h-full pt-20 flex items-center justify-center overflow-hidden relative shadow-inner-[20px] '>
+        <div
+            className=' h-full pt-20 flex items-center justify-center overflow-hidden relative shadow-inner-[20px] '>
             <img className="absolute blur left-0 saturate-200" src="/img-1.png" alt="" />
             <img className="absolute blur right-0 top-20 saturate-200" src="/img-3.png" alt="" />
             <img className="absolute blur left-0 right-0 saturate-0 w-full" src="/img-2.png" alt="" />
-            {/* <div className="h-[40rem] w-[40rem] bg-black rounded-full"></div> */}
-            {/* <div className="h-full p-40 "> */}
-            {/* <div className="bg-primary-pattern rounded-xl border border-primary-foreground  shadow-lg flex items-center relative justify-center h-full z-0"> */}
-                <div className="flex space-x-4 items-center justify-center z-10">
-                    <img className="h-[10rem]" src={logo} alt="" />
-                    <h1 className='text-7xl font-bold'>Threads</h1>
-                {/* </div> */}
-            {/* </div> */}
+
+            <div className="flex space-x-4 items-center justify-center z-10">
+                <img className="h-[10rem]" src={logoUrl} alt="" />
+                <h1 className='text-7xl font-bold'>Threads</h1>
             </div>
         </div>
     )
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import {  useNavigate } from 'react-router-dom'; // Use the appropriate router library
 import { Button } from './ui/button';
+import { ModeToggle } from './ui/modeToggler';
+import { useTheme } from './theme-provider';
 
 const Navbar = () => {
   const navigate = useNavigate()
+  const {theme} = useTheme()
   const isLoggedIn = false
   return (
-    <div className="flex fixed top-0 w-full border-b border-border justify-between items-center h-20  px-10 backdrop-blur-lg text-white z-50">
+    <div className={`flex fixed top-0 w-full border-b border-border justify-between items-center h-20  px-10 backdrop-blur-lg ${theme === "light" ? "bg-white" : ""}  text-white z-50`}>
       
-        <h1 onClick={() => {navigate("/")}} className="font-bold text-2xl cursor-pointer">Threads</h1>
+        <h1 onClick={() => {navigate("/")}} className="font-bold text-2xl cursor-pointer text-black dark:text-white ">Threads</h1>
       
       <div className="flex items-center justify-center">
         {isLoggedIn ? (
@@ -16,9 +19,10 @@ const Navbar = () => {
             <a href="/profile">Profile</a>
           </>
         ) : (
-          <div className='space-x-2'>
+          <div className='space-x-2 flex items-center justify-center'>
             <Button onClick={() =>{ navigate('/auth/login')}} variant={'secondary'}>Login</Button>
             <Button onClick={() => {navigate('/auth/signup')}}>Sign Up</Button>
+            <ModeToggle/>
           </div>
         )}
       </div>
