19be61a1a8168f76a3cd04ecad0641350c7839b6
Aayush Yadav <yadavaayush002@gmail.com>
2024-01-07 22:42:28 +0530

    removed unused dependency

--- client/src/hooks/Init.tsx
+++ client/src/hooks/Init.tsx
@@ -1,6 +1,6 @@
 import { profileState, userState } from '@/store'
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useSetRecoilState } from 'recoil'
 
 const Init = () => {
