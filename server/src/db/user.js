6204ff0785a3bee4f6db39290da56461e794ba8a
Aayush Yadav <yadavaayush002@gmail.com>
2024-11-21 23:29:24 +0530

    deleted js files created by TS

--- server/src/db/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var userSchema = new mongoose_1.default.Schema({
-    email: String,
-    password: String,
-});
-var User = mongoose_1.default.model("User", userSchema);
-exports.default = User;
